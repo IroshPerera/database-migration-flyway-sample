b54b20897a2463bed521f32f2036c7c4b9accd40
Irosh Perera <irosh@webmotech.com>
2024-09-13 10:40:10 +0530

    implement book controller

---
+++ src/main/java/com/webmotech/database_migration_flyway_sample_project/controller/BookController.java
@@ -0,0 +1,35 @@
+package com.webmotech.database_migration_flyway_sample_project.controller;
+
+import com.webmotech.database_migration_flyway_sample_project.dto.BookDTO;
+import com.webmotech.database_migration_flyway_sample_project.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/book")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping("/health")
+    public String health() {
+        return "Book Service is up and running";
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveBook(@RequestBody BookDTO bookDTO, BindingResult bindResult) {
+        if (bindResult.hasErrors()) {
+            return ResponseEntity.badRequest().body("Invalid Request");
+        }
+
+        try {
+            return ResponseEntity.ok(bookService.saveBook(bookDTO));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+}
