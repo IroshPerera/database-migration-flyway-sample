967800cd9649e5c574fdd73f309dd92c710153e8
Irosh Perera <irosh@webmotech.com>
2024-09-13 10:39:41 +0530

    init commit

---
+++ src/main/java/com/webmotech/database_migration_flyway_sample_project/DatabaseMigrationFlywaySampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.webmotech.database_migration_flyway_sample_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMigrationFlywaySampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMigrationFlywaySampleProjectApplication.class, args);
+	}
+
+}
